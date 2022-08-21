34085e09f6dc2739d96c3b9c15bdd352b6807ecb
epcrkumara <76660448+epcrkumara@users.noreply.github.com>
2022-08-21 09:28:37 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+from compare_prices import compare_prices 
+laughs_bread = 'https://scrape-sm1.github.io/site1/Crimson%20Bread%20Sliced%20market1super.com.html'
+glomark_bread = 'https://glomark.lk/top-crust-bread/p/13676'
+compare_prices(laughs_bread,glomark_bread)
